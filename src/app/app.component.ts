1d390ebadc036ef5049ce9bf38482c76bfb8891e
Carlos Delfino - Sony 1 <consultoria@carlosdelfino.eti.br>
2019-07-13 19:13:20 -0300

    Diversos ajustes para testes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'webservices';
+  title = 'Islamic Works WebServices';
 }
